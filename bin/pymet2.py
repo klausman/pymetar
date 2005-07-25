8e6151c6773fae2fe26d6e40cceaa9207a10af17
Tobias Klausmann <klausman@schwarzvogel.de>
2005-07-25 17:22:38 +0000

    Turned the script into something more reminiscent of a unit test.
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/pymetar@38 777d060a-bcfc-0310-a0f0-c4626ecb2d45

---
+++ bin/pymet2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python -tt
+import pymetar
+rf=pymetar.ReportFetcher("NZCM")
+rep=rf.FetchReport()
+rp=pymetar.ReportParser()
+pr=rp.ParseReport(rep)
+
+print "------- Full report -------"
+print pr.fullreport
+print "----- End full report -----"
+
+print "------ Parsed Values ------"
+for k in pr.__dict__.keys():
+ if k != "fullreport":
+  print "%s: %s"%(k,pr.__dict__[k])
+print "---- End Parsed Values ----"
+
+print "--- Report getFunctions ---"
+print "getTemperatureCelsius():",pr.getTemperatureCelsius()
+print "getTemperatureFahrenheit():",pr.getTemperatureFahrenheit()
+print "getDewPointCelsius():",pr.getDewPointCelsius()
+print "getDewPointFahrenheit():",pr.getDewPointFahrenheit()
+print "getWindSpeed():",pr.getWindSpeed()
+print "getWindDirection():",pr.getWindDirection()
+print "getWindCompass():",pr.getWindCompass()
+print "getVisibilityKilometers():",pr.getVisibilityKilometers()
+print "getVisibilityMiles():",pr.getVisibilityMiles()
+print "getHumidity():",pr.getHumidity()
+print "getPressure():",pr.getPressure()
+print "getRawMetarCode():",pr.getRawMetarCode()
+print "getWeather():",pr.getWeather()
+print "getSkyConditions():",pr.getSkyConditions()
+print "getStationName():",pr.getStationName()
+print "getStationCity():",pr.getStationCity()
+print "getStationCountry():",pr.getStationCountry()
+print "getCycle():",pr.getCycle()
+print "getStationPosition():",pr.getStationPosition()
+print "getStationPositionFloat():",pr.getStationPositionFloat()
+print "getStationLatitude():",pr.getStationLatitude()
+print "getStationLatitudeFloat():",pr.getStationLatitudeFloat()
+print "getStationLongitude():",pr.getStationLongitude()
+print "getStationLongitudeFloat():",pr.getStationLongitudeFloat()
+print "getStationAltitude():",pr.getStationAltitude()
+print "getReportURL():",pr.getReportURL()
+print "getTime():",pr.getTime()
+print "getISOTime():",pr.getISOTime()
+print "getPixmap():",pr.getPixmap()
+print "- End Report getFunctions -"
+
