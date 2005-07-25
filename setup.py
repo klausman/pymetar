edee500f173b68ed950d9f48853a58b3c9128a78
Tobias Klausmann <klausman@schwarzvogel.de>
2005-07-25 16:50:09 +0000

    Added setup.py for distutils.
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/pymetar@11 777d060a-bcfc-0310-a0f0-c4626ecb2d45

---
+++ setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+#
+# pymetar
+# (c) 2002 Tobias Klausmann
+# (c) 2002 Jerome Alet <alet@librelogiciel.com>
+# You're welcome to redistribute this software under the
+# terms of the GNU General Public Licence version 2.0
+# or, at your option, any higher version.
+#
+# You can read the complete GNU GPL in the file COPYING
+# which should come along with this software, or visit
+# the Free Software Foundation's WEB site http://www.fsf.org
+#
+# $Id: $
+
+from distutils.core import setup
+
+import pymetar
+
+setup(name = "pymetar", version = pymetar.__version__,
+      licence = "GNU GPL",
+      description = pymetar.__doc__,
+      author = "Tobias Klausmann",
+      author_email = "klausman@tuts.net",
+      url = "http://unattached.i-no.de/english/linux.php",
+      packages= [ "" ],
+      scripts = [ "bin/pymetar" ])
