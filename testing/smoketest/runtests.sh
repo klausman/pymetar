eff2510f1814cc8237282367d2f7fe70bdaf3575
Tobias Klausmann <klausman@schwarzvogel.de>
2010-10-31 08:41:06 +0000

    Added log dir More structure to test datasets Changes to runtest to work with new layout
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/pymetar@113 777d060a-bcfc-0310-a0f0-c4626ecb2d45

--- testing/smoketest/runtests.sh
+++ testing/smoketest/runtests.sh
@@ -7,10 +7,10 @@ SETS="set-2007-10-14  set-2010-01-17 set-2010-10-31"
 for TEST in $TESTS; do
 	for SET in $SETS; do
 		echo "Running $TEST on $SETDIR/$SET"
-		python $TEST $SETDIR/$SET > $TEST-$SET.log
+		python $TEST $SETDIR/$SET > logs/$TEST-$SET.log
 		if [ $? != 0 ]; then
 			echo "$TEST on $SETDIR/$SET failed" >&2
-			echo "See $TEST-$SET.log for details"
+			echo "See logs/$TEST-$SET.log for details"
 			break
 		fi
 	done
