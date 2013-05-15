eeb19af205bf7d6ee57b1ced06b1c701de506946
Tobias Klausmann <klausman@schwarzvogel.de>
2013-05-15 14:12:48 +0200

    Fix README, module docstring and regenerate librarydoc.txt
    The README is now better ReSt to help with displaying it on PyPI. In a
    similar vein, the module-level docstring is now less boilerplate-y
    (moved the GPL info and copyright banner to comments). Finally,
    regenerate librarydoc.txt to reflect dostring changes.

--- pymetar.py
+++ pymetar.py
@@ -1,25 +1,25 @@
 # -*- coding: utf8 -*-
 # pylint: disable-msg=C0103 # Disable naming style messages
-"""Pymetar (c) 2002-2012 Tobias Klausmann
-
-Pymetar is a python module and command line tool designed to fetch Metar
+# Pymetar (c) 2002-2012 Tobias Klausmann
+"""
+PyMETAR is a python module and command line tool designed to fetch Metar
 reports from the NOAA (http://www.noaa.gov) and allow access to the
 included weather information.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2 of the License, or (at your
-option) any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA."""
-
+"""
+# This program is free software; you can redistribute it and/or modify it
+#under the terms of the GNU General Public License as published by the
+#Free Software Foundation; either version 2 of the License, or (at your
+#option) any later version.
+#
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along
+#with this program; if not, write to the Free Software Foundation, Inc.,
+#51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA."""
+#
 import fpformat
 import math
 import re
