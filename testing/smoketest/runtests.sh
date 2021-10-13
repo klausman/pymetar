10b024bacebc67a7573e6528e16227957b403aa8
Bruce Duncan <bwduncan@gmail.com>
2021-10-13 20:12:26 +0100

    Create log dir for tests.

--- testing/smoketest/runtests.sh
+++ testing/smoketest/runtests.sh
@@ -4,6 +4,7 @@ TESTS="testall.py testpixmap.py testcloud.py testskycond.py"
 SETDIR="reports/"
 SETS="set-2007-10-14  set-2010-01-17 set-2010-10-31 set-2017-07-29"
 
+mkdir -p logs
 for TEST in $TESTS; do
 	for SET in $SETS; do
 		echo "Running $TEST on $SETDIR/$SET"
